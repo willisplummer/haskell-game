917b8e233f71df70790d30ad125741450edf38f2
willis plummer <willisplummer@gmail.com>
2020-02-10 21:33:15 -0500

    initial commit

---
+++ shell.nix
@@ -0,0 +1,28 @@
+let
+  pkgs = import <nixpkgs> { };
+
+  ghcVersion = "ghc865";
+
+  haskellPackages = pkgs.haskell.packages.${ghcVersion}.override { 
+    overrides = self: super: {
+      # If you need a particular version of a package instead
+      # of the default that nixpkgs provides, specify it here.
+    };
+  };
+
+  ghcide =
+    let 
+      url = "https://github.com/hercules-ci/ghcide-nix/tarball/master";
+    in
+      (import (builtins.fetchTarball url) {})."ghcide-${ghcVersion}";
+
+
+  oldDerivation = (import ./release.nix).project.env;
+
+in pkgs.stdenv.mkDerivation {
+  name = "env";
+  buildInputs = [
+    haskellPackages.ghcid
+    ghcide
+  ];
+}
