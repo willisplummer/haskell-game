107dbb9c387942dfaaad285b07b5f6637223c6d2
willis plummer <willisplummer@gmail.com>
2020-02-13 17:08:19 -0500

    more nix shell experimentation

--- shell.nix
+++ shell.nix
@@ -35,21 +35,34 @@
 # }
 
 with import <nixpkgs> {};
-mkShell {
-    buildInputs = [
-        glfw
-        ftgl
-        freealut
-        (haskell.packages.ghc865.ghcWithPackages (pkgs: [
-            pkgs.gloss
-            pkgs.GLFW-b
-        ]))
-    ];
-    shellHook = ''
-      expression=$(grep "export" < nixGL/result/bin/nixGL*)
-      if [ -n "$expression" ]; then
-        eval "$expression"
-      fi
-    '';
-}
+let
+  stack-1_9_3 =
+    (import (builtins.fetchTarball {
+      url = "https://github.com/NixOS/nixpkgs/archive/bc94dcf500286495e3c478a9f9322debc94c4304.tar.gz";
+      sha256 = "1siqklf863181fqk19d0x5cd0xzxf1w0zh08lv0l0dmjc8xic64a";
+    }) { }).stack;
+
+in
+  mkShell {
+      buildInputs = [
+          clang
+          glfw
+          ftgl
+          freealut
+          gcc
+          stack-1_9_3
+          (haskell.packages.ghc865.ghcWithPackages (pkgs: [
+              pkgs.gloss
+              pkgs.GLFW-b
+              pkgs.gloss-rendering
+              pkgs.mtl
+          ]))
+      ];
+      shellHook = ''
+        expression=$(grep "export" < nixGL/result/bin/nixGL*)
+        if [ -n "$expression" ]; then
+          eval "$expression"
+        fi
+      '';
+  }
 
