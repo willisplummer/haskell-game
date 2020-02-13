278c12dcb91edaaf9365e393a9f7d041be71e866
willis plummer <willisplummer@gmail.com>
2020-02-12 21:54:26 -0500

    ghcide cachix (not working yet)

--- README.md
+++ README.md
@@ -4,6 +4,12 @@ to run locally:
 
 - install stack
 - install nix-shell
+
+```
+$ nix-env -iA cachix -f https://cachix.org/api/v1/install
+$ cachix use ghcide-nix
+```
+
 - open a nix shell (`nix-shell`)
 - in the shell, run `stack build`
 - in the shell, run `stack exec haskell-game-exe`
--- shell.nix
+++ shell.nix
@@ -1,13 +1,29 @@
-with import <nixpkgs> {};
-mkShell {
+let
+  pkgs = import <nixpkgs> {};
+
+  ghcVersion = "ghc863";
+
+  haskellPackages = pkgs.haskell.packages.${ghcVersion}.override { 
+    overrides = self: super: {
+      # If you need a particular version of a package instead
+      # of the default that nixpkgs provides, specify it here.
+    };
+  };
+
+  ghcide =
+    let 
+      url = "https://github.com/cachix/ghcide-nix/tarball/master";
+    in
+      (import (builtins.fetchTarball url) {})."ghcide-${ghcVersion}";
+
+in pkgs.stdenv.mkDerivation {
+    name = "env";
     buildInputs = [
-        glfw3
-        ftgl
-        freealut
-        (haskell.packages.ghc865.ghcWithPackages (pkgs: [
-            pkgs.gloss
-            pkgs.GLFW-b
-        ]))
+        pkgs.glfw3
+        pkgs.ftgl
+        pkgs.freealut
+        haskellPackages.ghcid
+        ghcide
     ];
     shellHook = ''
       expression=$(grep "export" < nixGL/result/bin/nixGL*)
