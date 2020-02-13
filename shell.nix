49bc16c3fedff08a9306f5e18d2b22e92d99a810
willis plummer <willisplummer@gmail.com>
2020-02-13 00:16:34 -0500

    almost working nix-shell

--- shell.nix
+++ shell.nix
@@ -1,29 +1,49 @@
-let
-  pkgs = import <nixpkgs> {};
+# let
+#   pkgs = import <nixpkgs> {};
 
-  ghcVersion = "ghc863";
+#   ghcVersion = "ghc865";
 
-  haskellPackages = pkgs.haskell.packages.${ghcVersion}.override { 
-    overrides = self: super: {
-      # If you need a particular version of a package instead
-      # of the default that nixpkgs provides, specify it here.
-    };
-  };
+#   haskellPackages = pkgs.haskell.packages.${ghcVersion}.override { 
+#     overrides = self: super: {
+#       # If you need a particular version of a package instead
+#       # of the default that nixpkgs provides, specify it here.
+#     };
+#   };
 
-  ghcide =
-    let 
-      url = "https://github.com/cachix/ghcide-nix/tarball/master";
-    in
-      (import (builtins.fetchTarball url) {})."ghcide-${ghcVersion}";
+#   ghcide =
+#     let 
+#       url = "https://github.com/cachix/ghcide-nix/tarball/master";
+#     in
+#       (import (builtins.fetchTarball url) {})."ghcide-${ghcVersion}";
 
-in pkgs.stdenv.mkDerivation {
-    name = "env";
+# in pkgs.stdenv.mkDerivation {
+#     name = "env";
+#     buildInputs = [
+#         pkgs.ghc
+#         pkgs.glfw3
+#         pkgs.ftgl
+#         pkgs.freealut
+#         haskellPackages.ghcid
+#         # ghcide
+#     ];
+#     shellHook = ''
+#       expression=$(grep "export" < nixGL/result/bin/nixGL*)
+#       if [ -n "$expression" ]; then
+#         eval "$expression"
+#       fi
+#     '';
+# }
+
+with import <nixpkgs> {};
+mkShell {
     buildInputs = [
-        pkgs.glfw3
-        pkgs.ftgl
-        pkgs.freealut
-        haskellPackages.ghcid
-        ghcide
+        glfw
+        ftgl
+        freealut
+        (haskell.packages.ghc865.ghcWithPackages (pkgs: [
+            pkgs.gloss
+            pkgs.GLFW-b
+        ]))
     ];
     shellHook = ''
       expression=$(grep "export" < nixGL/result/bin/nixGL*)
@@ -32,3 +52,4 @@ in pkgs.stdenv.mkDerivation {
       fi
     '';
 }
+
